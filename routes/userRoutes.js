4b01d259c0b5eebe5ca40582d78da66c603bb2d6
umairtouseef <umairtuseef41@gmail.com>
2025-01-15 03:35:30 -0800

    new

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,16 +7,16 @@ const router = express.Router();
 
 router.post('/signup', userController.createUser);
 router.post('/login', passport.authenticate('local'), userController.loginUser);
-router.get('/profile', passport.authenticate("jwt"), userController.checkAuth);
+router.get('/protected-route', passport.authenticate("jwt"), userController.checkAuth);
 
   
-router.get('/protected-route',sessionAuth, userController.checkAuth, (req, res) => {
-    res.status(200).json({
-      success: true,
-      message: "Access granted to protected route",
-      user: req.user,
-    });
-  });
+// router.get('/protected-route',sessionAuth, userController.checkAuth, (req, res) => {
+//     res.status(200).json({
+//       success: true,
+//       message: "Access granted to protected route",
+//       user: req.user,
+//     });
+//   });
 router.get('/logout', userController.logout);
 router.post("/reset-password-request", userController.resetPasswordRequest)
 router.post("/reset-password", userController.resetPassword);
